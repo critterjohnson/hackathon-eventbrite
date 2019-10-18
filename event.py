5b15b38ad0c9f1d28a5674d69ec6f33a30eb1dad
Christopher Johnson <36961199+critterjohnson@users.noreply.github.com>
2019-10-18 10:28:10 -0400

    funtions to query eventbrite api and save to excel

---
+++ event.py
@@ -0,0 +1,38 @@
+# event.py - functions to use eventbrite api
+import requests
+import json
+from eventbritexl import save_attendees
+
+# returns a list of attendees, handles pagination automatically
+def get_attendee_list(event_id:str, token:str, continuation:str=None):
+    endpoint = f"https://www.eventbriteapi.com/v3/events/{event_id}/attendees"
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    params = None
+    if continuation is not None:
+        params = {
+            "continuation": continuation
+        }
+    response = requests.get(endpoint, headers=headers, params=params)
+
+    if response.status_code != 200:
+        raise ValueError(f"Bad response: {response.status_code}")
+
+    resp_json = response.json()
+    att_list = resp_json["attendees"]
+    pagination = resp_json["pagination"]
+    if pagination["has_more_items"]:
+        att_list.extend(get_attendee_list(event_id, 
+                                          token, 
+                                          pagination["continuation"]))
+
+    return att_list
+
+if __name__ == "__main__":
+    with open("options.json", "r") as file:
+        options = json.load(file)
+    token = options["api_key"]
+    event_id = options["event_id"]
+    att_list = get_attendee_list(event_id, token)
+    save_attendees(att_list, "attendees.xlsx")
---
+++ eventbritexl.py
@@ -0,0 +1,47 @@
+# eventbritexl.py - saves eventbrite data to excel spreadsheets
+from openpyxl import Workbook
+
+def save_attendees(attendees:list, filename:str):
+    wb = Workbook()
+    wp = wb.active
+
+    row = 1
+    col = 1
+    
+    # get questions
+    ids = []
+    profile_opts = []
+    first_att = attendees[len(attendees) - 1]
+    for key in first_att["profile"].keys():
+        profile_opts.append(key)
+        wp.cell(row = row, column=col).value = key
+        col += 1
+    for question in first_att["answers"]:
+        wp.cell(row=row, column=col).value = question["question"]
+        ids.append(question["question_id"])
+        col += 1
+    col = 1
+    row += 1
+
+    # save responses
+    for attendee in attendees:
+        for opt in profile_opts:
+            try:
+                wp.cell(row=row, column=col).value = str(attendee["profile"][opt])
+            except KeyError:
+                wp.cell(row=row, column=col).value = "_"
+            col += 1
+        i = 0
+        for question in attendee["answers"]:
+            try:
+                if question["question_id"] != ids[i]:
+                    wp.cell(row=row, column=col).value = question["answer"]
+                    i += 1
+                else:
+                     wp.cell(row=row, column=col).value = "_"
+            except KeyError:
+                wp.cell(row=row, column=col).value = "_"
+            col += 1
+        col = 1
+        row += 1
+    wb.save(filename)
